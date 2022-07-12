9934e1f2a01ad6d6859094f50e2a10608be09d06
Qixin WANG <wchshapp_od@icloud.com>
2022-07-12 18:12:58 +0800

    Start to provide C implementation of cblas_sgemv.

--- CBLAS/src/cblas_sgemv.c
+++ CBLAS/src/cblas_sgemv.c
@@ -9,8 +9,108 @@
 #include "cblas.h"
 #include "cblas_f77.h"
 
-void soccs_sce_sgemv(&TA, &M, &N, &alpha, A, &lda, X, &incX,
-                &beta, Y, &incY){
+float max(const float a, const float b){
+   return ((a) > (b))?(a):(b);
+}
+
+char lsame(const char c, const char l){
+   //Start of concern >>>
+   //Here we assume ASCII encoding. The reference BLAS implementation
+   //assumes other encoding also.
+   //<<< End of concern
+
+   //Start of to do >>>
+   //Please insert parameter sanity checks here.
+   //e.g. assert('A' <= (*l) <= 'Z')
+   //<<< End of to do
+
+   if ((c) == (l)) return 1;
+   else if ('a' <= (c) && (c) <= 'z' && ((c) - ('a' - 'A')) == (l)) return 1;
+   else if ('A' <= (c) && (c) <= 'Z' && ((c) + ('a' - 'A')) == (l)) return 1;
+   else return 0;
+} 
+
+void soccs_sce_sgemv(const CBLAS_TRANSPOSE trans, const CBLAS_INT m, const CBLAS_INT n, 
+      const float alpha, const float *A, const CBLAS_INT lda, 
+      const float *X, const CBLAS_INT inc_X, const float beta, 
+      float *Y, const CBLAS_INT inc_Y){
+   
+   //floating point number 1 is not 1.0e+0, 0 is not 0.0e+0
+   const float ONE = 1.0e+0, ZERO = 0.0e+0;
+
+   //local scalars
+   float tmp;
+   int i, info, i_X, i_Y, j, j_X, j_Y, k_X k_Y, len_X, len_Y;
+
+   //Start of to do >>>
+   //Please insert parameter sanity checks here.
+   //<<< End of to do
+
+   //Quick return if possible.
+   if (m == 0 || n == 0 || ((alpha == ZERO) && (beta == ONE))) return;
+
+   if (lsame(trans, 'N')){
+      len_X = n;
+      len_Y = m;
+   }else{
+      len_X = m;
+      len_Y = n;
+   }
+
+   //Think X is a row of a column major stored matrix, 
+   //inc_X > 0 is the number of elements in a column
+   //-inc_X is the number of elements in a column when enumerating X from back to front element.
+   if (inc_X > 0){
+      k_X = 0;
+   }else{
+      k_X = 0 - (len_X - 1) * inc_X;
+   }
+
+   if (inc_Y > 0){
+      k_Y = 0;
+   }else{
+      k_Y = 0 - (len_Y - 1) * inc_Y;
+   }
+
+   //First assign $Y := \beta Y$.
+   if (beta != ONE){
+      if (inc_Y == 1){
+         if (beta == ZERO)
+            for (int i = 0; i < len_Y; i++) Y[i] = ZERO;
+         else
+            for (int i = 0; i < len_Y; i++) Y[i] *= beta;
+      }else{
+         i_Y = k_Y;
+         if (beta == ZERO){
+            for (int i = 0; i < len_Y; i++){
+               Y[i_Y] = ZERO;
+               i_Y += inc_Y;
+            }
+         }else{
+            for (int i = 0; i < len_Y; i++){
+               Y[i_Y] *= beta;
+               i_Y += inc_Y;
+            }
+         }
+      }
+   }
+   //Now $Y := \beta Y$.
+
+   //Next assign $Y := \alpha A X + Y$ or $Y := \alpha A^T X + Y$.
+   if (alpha == ZERO) return;
+   if (lsame(trans, 'N')){
+      //$Y := \alpha A X + Y$.
+
+      //reached here on 20220712.
+
+
+
+
+
+   
+
+
+   
    //SoCCS SCE client thread code
    //copy data to a req packet in the shared memory circular queue
    //send req packet to server stack server thread
@@ -43,8 +143,8 @@ void cblas_sgemv(const CBLAS_LAYOUT layout,
          CBLAS_CallFromC = 0;
          RowMajorStrg = 0;
       }
-      soccs_sce_sgemv(&TA, &M, &N, &alpha, A, &lda, X, &incX,
-                &beta, Y, &incY);
+      soccs_sce_sgemv(TA, M, N, alpha, A, lda, X, incX,
+                beta, Y, incY);
    }
    else if (layout == CblasRowMajor)
    {
@@ -59,8 +159,8 @@ void cblas_sgemv(const CBLAS_LAYOUT layout,
          RowMajorStrg = 0;
          return;
       }
-      soccs_sce_sgemv(&TA, &M, &N, &alpha, A, &lda, X, &incX,
-                &beta, Y, &incY);
+      soccs_sce_sgemv(TA, M, N, alpha, A, lda, X, incX,
+                beta, Y, incY);
    }
    else cblas_xerbla(1, "cblas_sgemv", "Illegal layout setting, %d\n", layout);
    CBLAS_CallFromC = 0;
