95bc080fddc67bf803c7b7415fb31aa27304207b
xyliax <20083743d@connect.polyu.hk>
2022-12-15 10:05:45 +0800

    updatew

--- test/main.c
+++ test/main.c
@@ -5,18 +5,20 @@
 int main(int argc, char *argv[]) {
 	CBLAS_LAYOUT 	layout 	= CblasRowMajor;
 	CBLAS_TRANSPOSE	TransA	= CblasNoTrans;
-	CBLAS_INT		M		= atoi(argv[1]);
-	CBLAS_INT		N		= atoi(argv[2]);
-	float			alpha	= 1.0;
-	float 			*A 		= (float*) malloc (sizeof(float) * M * N);
-	CBLAS_INT 		lda		= N;
+	FILE *input = fopen("test.in", "r");
+	CBLAS_INT	M, N;
+	fscanf(input, "%d", &M);
+	fscanf(input, "%d", &N);
 
+	float	alpha	= 1.0;
+	float *A 	= (float*) malloc (sizeof(float) * M * N);
+	CBLAS_INT lda	= N;
 	int len_X = 0, len_Y = 0;
 
-	if (TransA == CblasNoTrans){
+	if (TransA == CblasNoTrans) {
 		len_X = N;
 		len_Y = M;
-	}else{
+	} else {
 		len_X = M;
 		len_Y = N;
 	}
@@ -25,30 +27,28 @@ int main(int argc, char *argv[]) {
 	CBLAS_INT		incX	= 1;
 	float			beta	= 1.0;
 	float			*Y		= (float*) malloc (sizeof(float) * len_Y);
+	float			*TY		= (float*) malloc (sizeof(float) * len_Y);
 	CBLAS_INT		incY	= 1;
 	/*
-	float Ac[] = {	1.0, 1.0, 1.0,
-					1.0, 1.0, 1.0,
-					1.0, 1.0, 1.0,
-					1.0, 1.0, 1.0,
-					1.0, 1.0, 1.0};
-	memcpy(A, Ac, sizeof(Ac));
-	float Xc[] = {1.0, 1.0, 1.0};
-	memcpy(X, Xc, sizeof(Xc));
-	float Yc[] = {1.0, 1.0, 1.0, 1.0, 1.0};
-	memcpy(Y, Yc, sizeof(Yc));
-	*/
+		 float Ac[] = {	1.0, 1.0, 1.0,
+		 1.0, 1.0, 1.0,
+		 1.0, 1.0, 1.0,
+		 1.0, 1.0, 1.0,
+		 1.0, 1.0, 1.0};
+		 memcpy(A, Ac, sizeof(Ac));
+		 float Xc[] = {1.0, 1.0, 1.0};
+		 memcpy(X, Xc, sizeof(Xc));
+		 float Yc[] = {1.0, 1.0, 1.0, 1.0, 1.0};
+		 memcpy(Y, Yc, sizeof(Yc));
+	 */
 	int vi = 3;
-	for(int i = 0; i < N * M; i++)
-		A[i] = atof(argv[vi++]);
-	for(int i = 0; i < len_X; i++)
-		X[i] = atof(argv[vi++]);
-	for(int i = 0; i < len_Y; i++)
-		Y[i] = atof(argv[vi++]);
-
-	cblas_sgemv(layout, TransA, M, N, alpha, A, lda, X, incX, beta, Y, incY);
-
-	for(int i = 0; i < len_Y; i++)
-		printf("%f\n", Y[i]); 
+	for(int i = 0; i < N * M; i++) fscanf(input, "%f", &A[i]);
+	for(int i = 0; i < len_X; i++) fscanf(input, "%f", &X[i]);
+	for(int i = 0; i < len_Y; i++) fscanf(input, "%f", &Y[i]);
+	while(1) {
+		memcpy(TY, Y, sizeof(float) * len_Y);
+		cblas_sgemv(layout, TransA, M, N, alpha, A, lda, X, incX, beta, Y, incY);
+		printf("Finish 1 round!\n");
+	}
 	return 0;
 }
